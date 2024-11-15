aa7b379d07ae8dcf24ba7a50dfe8acbd868bf1f2
Charity Samuel <scharity463@gmail.com>
2024-11-15 21:35:54 +0300

    deployment

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect("mongodb+srv://beez:inthetrap@users.pelvm.mongodb.net/?retryWrites=true&w=majority&appName=users");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
